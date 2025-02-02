1252ca2709f4383e71b5dcda98d6d5ef2594c7e1
Muhammad-Basas-Tahir <muhammadahmed1233212017@gmail.com>
2025-02-03 00:37:04 +0500

    react doc ex1 of component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,8 @@
-
-import './App.css'
-
-function Profile() {
+export default function Profile() {
   return (
-    <div>
-      
-
-      <img src="/example2.jpg" className='w-40'/>
-      <img src="google_logo.png" />
-    </div>
-  )
-}
-
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-        <Profile />
-        <Profile />
-        <Profile />
-    </section>
+    <img
+      src="https://i.imgur.com/lICfvbD.jpg"
+      alt="Aklilu Lemma"
+    />
   );
 }
